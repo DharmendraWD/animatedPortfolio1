e938a86b7fe86bb1b66e452c50e570d8803db8b0
DharmendraWD <zaynhere@icloud.com>
2024-04-10 20:53:52 +0545

    all animation set

--- script.js
+++ script.js
@@ -46,6 +46,72 @@ function realPageAnimation(){
     })
     
 }
+function teamAnimation(){
+    document.querySelectorAll(".listelem")
+.forEach(function(el){
+    el.addEventListener("mousemove", function(dets){
+     
+        gsap.to(this.querySelector(".picture"), {opacity: 1, x:   gsap.utils.mapRange(0, window.innerWidth, -200, 200, dets.clientX ) , ease: Power4, duration: .5})
+    })
+
+    el.addEventListener("mouseleave", function(dets){
+        gsap.to(this.querySelector(".picture"), {opacity: 0, ease: Power4, duration: .5})
+    })
+
+
+})
+}
+function paraAnimation(){
+    var clutter = '';
+document.querySelector(".textPara")
+.textContent.split("")
+.forEach(function(e){
+    if(e=== " ") clutter += `<span>&nbsp;</span>`
+    clutter += `<span> ${e} </span>`
+})
+
+document.querySelector(".textPara").innerHTML=clutter;
+gsap.set(".textPara span", {opacity: .1})
+gsap.to(".textPara span", {
+    scrollTrigger: {
+        trigger: ".para",
+        start: "top 60%",
+        end: "bottom 90%",
+        scrub: 1
+    }, 
+    opacity: 1, 
+    stagger: .03,
+    ease: Power4
+})
+
+}
+function capsuleAnimation(){
+    gsap.to(".capsule:nth-child(2)", {
+        scrollTrigger:{
+            trigger: ".capsules", 
+            start: "top 70%",
+            end: "bottom bottom",
+            scrub: 1
+        },
+        y: 0,
+        ease: Power4
+    })
+}
+
+document.querySelectorAll(".section")
+.forEach(function(e){
+    ScrollTrigger.create({
+        trigger: e,
+        start: "top 50%",
+        end: "bottom 50%",
+        onEnter: function(){
+document.body.setAttribute("theme", e.dataset.color)
+        },
+        onEnterBack: function(){
+            document.body.setAttribute("theme", e.dataset.color)
+        }
+    })
+})
 
 
 
@@ -53,3 +119,12 @@ function realPageAnimation(){
 
  homePageAnimation()
  realPageAnimation()
+ teamAnimation()
+ paraAnimation()
+ capsuleAnimation()
+
+
+//  locomotive js 
+ (function () {
+    const locomotiveScroll = new LocomotiveScroll();
+})();
